d74ed45b22e3d4be903596f3ff93734f7046c204
userofcrypto <95953820+userofcrypto@users.noreply.github.com>
2026-04-24 14:56:17 -0400

    Simplify Home component to display running status
    Removed command input and response handling from Home component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,3 @@
-import { useState } from "react";
-
 export default function Home() {
-  const [command, setCommand] = useState("");
-  const [response, setResponse] = useState("");
-
-  const sendCommand = async () => {
-    const res = await fetch("/api/command", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ command })
-    });
-
-    const data = await res.json();
-    setResponse(JSON.stringify(data, null, 2));
-  };
-
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Factory System</h1>
-
-      <input
-        style={{ width: 400, padding: 10 }}
-        value={command}
-        onChange={(e) => setCommand(e.target.value)}
-        placeholder="Type command..."
-      />
-
-      <button onClick={sendCommand} style={{ marginLeft: 10 }}>
-        Send
-      </button>
-
-      <pre style={{ marginTop: 20 }}>{response}</pre>
-    </div>
-  );
+  return <div>Factory System Running</div>;
 }
